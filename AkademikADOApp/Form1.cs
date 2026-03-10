cc5c32048a41bbaef7853d5c07709207eff03eb8
dev0062 <zaindevalldino@gmail.com>
2026-03-10 11:57:01 +0700

    Menambahkan blok try-catch pada btnConnect

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -30,7 +30,14 @@ namespace AkademikADOApp
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
-           
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                      
+            }
 
         }
 
